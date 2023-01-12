b1b24225e12e46b8ed4f22caf72a48a362e47f45
Ronald M. Kasendwa <kasendwaronald@gmail.com>
2023-01-12 04:11:09 +0300

    Refactors api adapter

--- src/Adapter/index.ts
+++ src/Adapter/index.ts
@@ -8,6 +8,8 @@ import {
   queueRequest,
 } from './RequestQueue';
 
+export { RequestOptions, ResponseProcessor };
+
 export const REDIRECTION_ERROR_MESSAGES = [
   'User session timed out',
   'Session timed out',
